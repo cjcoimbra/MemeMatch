d8d2183c6f0222460b4271da1fa25e8491a6626a
Christian Coimbra <dm.black@gmail.com>
2017-12-15 00:26:34 -0200

    useless data

--- get_ranking.php
+++ get_ranking.php
@@ -2,8 +2,8 @@
 
 $game= $_GET['game'];
 
-mysql_connect( "mysql12.uni5.net", "spacecatstudio01", "a1b2c3d4e5" ) or die( mysql_error() );
-mysql_select_db( "spacecatstudio01" ) or die( mysql_error() );
+mysql_connect( "", "", "" ) or die( mysql_error() );
+mysql_select_db( "" ) or die( mysql_error() );
 
 $amount = 1;
 
