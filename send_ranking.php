7a9ef2fcc063bad7a29b79c8310ae0275867e2ff
Christian Coimbra <dm.black@gmail.com>
2017-12-15 00:26:02 -0200

    useless data

--- send_ranking.php
+++ send_ranking.php
@@ -3,8 +3,8 @@
 $player    = $_GET['username'];
 $score    = $_GET['score']; 
 
-mysql_connect( "mysql12.uni5.net", "spacecatstudio01", "a1b2c3d4e5" ) or die( mysql_error() );
-mysql_select_db( "spacecatstudio01" ) or die( mysql_error() );
+mysql_connect( "...", "...", "..." ) or die( mysql_error() );
+mysql_select_db( "..." ) or die( mysql_error() );
 
 
 $query = "INSERT INTO Ranking (Name, Score) VALUES ('".$player."', '".$score."')";
